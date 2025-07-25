fb875d4915606b49995180a26a1fb93a426867ef
carimbai <atendimento.carimbai@gmail.com>
2025-07-25 14:45:30 +0000

    style: use `prettier:fix` to style coding

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,20 +9,26 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response1 = await fetch("http://127.0.0.1:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://127.0.0.1:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response1.status).toBe(201);
 
         const response1Body = await response1.json();
 
         expect(Array.isArray(response1Body)).toBe(true);
-        expect(response1Body.length).toBeGreaterThan(0);        
+        expect(response1Body.length).toBeGreaterThan(0);
       });
       test("For the second time", async () => {
-        const response2 = await fetch("http://127.0.0.1:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://127.0.0.1:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response2.status).toBe(200);
 
         const response2Body = await response2.json();
